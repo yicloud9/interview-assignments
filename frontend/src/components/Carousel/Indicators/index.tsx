d0805e8bcea11ff8fb3caa134acafb6d34b3335a
yicloud <zxlegend@163.com>
2022-05-17 20:25:42 +0800

    feat: finish carousel

---
+++ frontend/src/components/Carousel/Indicators/index.tsx
@@ -0,0 +1,37 @@
+import React, {useRef} from 'react'
+
+import "./index.css"
+
+type CarouselIndicatorsProps = {
+  current: number,
+  count: number,
+  autoPlay: boolean,
+  onChange: (index: number) => void
+}
+
+const Indicators = (props: CarouselIndicatorsProps) => {
+  const {current, count, onChange, autoPlay} = props
+  const indicatorsRef = useRef<HTMLDivElement>(null)
+
+  if(count <= 1) {
+    return null
+  }
+
+  return (<div className="carousel__indicators" ref={indicatorsRef}>
+    {new Array(count).fill(true).map((_, index) => 
+      <div 
+        key={index}
+        onClick={() => onChange(index)}
+        className={current === index
+          ? 'carousel__indicator carousel__indicator-active' 
+          : 'carousel__indicator'}>
+            {autoPlay 
+              ? <div className="indicator__progress"></div>
+              : <div className="indicator__content"></div>
+            }
+          </div>
+    )}
+  </div>)
+}
+
+export default Indicators
